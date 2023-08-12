a7d4f105df49f5bb99ed09924b297a5a9a1b36ad
RemasMehyar <roromehyar5@gmail.com>
2023-08-12 17:45:44 +0300

    finished

---
+++ bubbleSort.rb
@@ -0,0 +1,15 @@
+def bubbleSort (array)
+    for i in 0...array.length
+        for j in 0...array.length-1
+            if array[j] > array[j+1]
+                temp = array[j+1]
+                array[j+1] = array[j]
+                array[j]=temp
+            end
+        end
+    end
+            array
+    end
+        p bubbleSort([4,3,78,2,0,2])
+
+
